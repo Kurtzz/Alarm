1060faff2d23c7b60e0617c488a2697e640efc3b
nachteil <nachteil@gmail.com>
2016-04-08 10:57:13 +0200

    Added resources, implementing GCS communication

---
+++ pocs/push-client/app/src/main/java/alarm/io/ki/agh/pl/push_poc/AlarmPreferences.java
@@ -0,0 +1,9 @@
+package alarm.io.ki.agh.pl.push_poc;
+
+
+public class AlarmPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}

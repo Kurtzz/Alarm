3e281041e602239da7863379eed426ae7ef23047
nachteil <nachteil@gmail.com>
2016-04-15 13:23:44 +0200

    Removed changes merged by mistake

--- pocs/push-client/app/src/main/java/alarm/io/ki/agh/pl/push_poc/AlarmPreferences.java
+++
@@ -1,9 +0,0 @@
-package alarm.io.ki.agh.pl.push_poc;
-
-
-public class AlarmPreferences {
-
-    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
-    public static final String REGISTRATION_COMPLETE = "registrationComplete";
-
-}

e793fc1f7acae4515e140185072a273fb4bdf580
nachteil <nachteil@gmail.com>
2016-04-20 21:43:16 +0200

    Started logging

--- pocs/push-client/app/src/main/java/alarm/io/ki/agh/pl/push_poc/MyInstanceIDListenerService.java
+++ pocs/push-client/app/src/main/java/alarm/io/ki/agh/pl/push_poc/MyInstanceIDListenerService.java
@@ -1,5 +1,7 @@
 package alarm.io.ki.agh.pl.push_poc;
 
+import android.util.Log;
+
 import com.google.android.gms.iid.InstanceID;
 import com.google.android.gms.iid.InstanceIDListenerService;
 
@@ -7,8 +9,9 @@ public class MyInstanceIDListenerService extends InstanceIDListenerService {
 
     public MyInstanceIDListenerService() {
         super();
+        Log.i(this.getClass().getSimpleName(), "Before getting instance id");
         String iid = InstanceID.getInstance(this).getId();
-
+        Log.i(this.getClass().getSimpleName(), "Received instance id: " + iid);
     }
 
     @Override

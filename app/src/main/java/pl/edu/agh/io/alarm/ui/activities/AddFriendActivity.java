7e0985859358afac6984a93631c51cc4f1afd7c1
Vlizer <vlizer3@gmail.com>
2016-04-21 15:21:08 +0200

    Creating ADDFRIEND Activity

--- app/src/main/java/pl/edu/agh/io/alarm/ui/activities/AddFriendActivity.java
+++ app/src/main/java/pl/edu/agh/io/alarm/ui/activities/AddFriendActivity.java
@@ -3,6 +3,8 @@ package pl.edu.agh.io.alarm.ui.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import pl.edu.agh.io.alarm.R;
 
@@ -15,6 +17,10 @@ public class AddFriendActivity extends Activity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addfriend);
+        Button btn = (Button) findViewById(R.id.ADDFIREND_addbtn);
+        btn.setOnClickListener(this);
+        ImageButton b = (ImageButton) findViewById(R.id.SENDMESSAGE_exitbtn);
+        b.setOnClickListener(this);
     }
 
     @Override

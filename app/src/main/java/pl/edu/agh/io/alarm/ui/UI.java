9bb34e6b6de18a94371ab41bd1f9224864c7c709
Vlizer <vlizer3@gmail.com>
2016-04-22 13:40:16 +0200

    Add Login Activities Implementation

--- app/src/main/java/pl/edu/agh/io/alarm/ui/UI.java
+++ app/src/main/java/pl/edu/agh/io/alarm/ui/UI.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import pl.edu.agh.io.alarm.ui.activities.SendMessageActivity;
+import pl.edu.agh.io.alarm.ui.activities.LoginActivity;
 
 /**
  * Created by Mateusz on 2016-04-21.
@@ -13,7 +13,7 @@ public class UI extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, SendMessageActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/pl/edu/agh/io/alarm/ui/activities/LoginActivity.java
+++ app/src/main/java/pl/edu/agh/io/alarm/ui/activities/LoginActivity.java
@@ -1,16 +1,49 @@
 package pl.edu.agh.io.alarm.ui.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import pl.edu.agh.io.alarm.R;
 
 /**
  * Created by Mateusz on 2016-04-22.
  */
 public class LoginActivity extends Activity implements View.OnClickListener {
 
-    
+
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_login);
+        Button send = (Button) findViewById(R.id.LOGINLoginbtn);
+        send.setOnClickListener(this);
+        System.out.println("LoginActivity : Create");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.LOGINLoginbtn:
+                EditText loginText = (EditText) findViewById(R.id.LOGINloginText);
+                EditText passwordText = (EditText) findViewById(R.id.LOGINpasswordText);
+                if(loginText != null && passwordText != null){
+                    System.out.println("Login: "+loginText.getText().toString());
+                    System.out.println("Login: " + passwordText.getText().toString());
+                    Intent intent = new Intent(this, SendMessageActivity.class);
+                    startActivity(intent);
+                }
+                break;
+        }
     }
+
 }

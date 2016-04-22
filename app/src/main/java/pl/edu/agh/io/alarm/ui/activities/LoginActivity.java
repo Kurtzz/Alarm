8be456f2bbfd9a7b336819aba174356089a08e7c
Vlizer <vlizer3@gmail.com>
2016-04-22 13:31:17 +0200

    Creat LoginActivity

---
+++ app/src/main/java/pl/edu/agh/io/alarm/ui/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.io.alarm.ui.activities;
+
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by Mateusz on 2016-04-22.
+ */
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    
+    @Override
+    public void onClick(View v) {
+
+    }
+}

96296ecf1a9b913c88633c51167ea68ec3d0cef9
Kurtzz <przemyslaw.kurc@gmail.com>
2016-04-23 11:36:13 +0200

    Add MessagesFragment

--- app/src/main/java/pl/agh/ki/io/alarm/alarm/MainActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/alarm/MainActivity.java
@@ -95,7 +95,10 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-
+            switch (position) {
+                case 0:
+                    return MessagesFragment.newInstance();
+            }
             return PlaceholderFragment.newInstance(position + 1);
         }
 

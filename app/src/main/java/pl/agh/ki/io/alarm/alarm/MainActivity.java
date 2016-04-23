16f451b6a1b0c6aca4c5d1ae3d1752fa8744b421
Kurtzz <przemyslaw.kurc@gmail.com>
2016-04-23 11:42:56 +0200

    Remove PlaceholderFragment

--- app/src/main/java/pl/agh/ki/io/alarm/alarm/MainActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/alarm/MainActivity.java
@@ -1,8 +1,6 @@
 package pl.agh.ki.io.alarm.alarm;
 
-import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -12,13 +10,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -103,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
                 case 2:
                     return GroupsFragment.newInstance();
             }
-            return PlaceholderFragment.newInstance(position + 1);
+
+            return null;
         }
 
         @Override
@@ -123,39 +117,6 @@ public class MainActivity extends AppCompatActivity {
                     return "GROUPS";
             }
             return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
         }
     }
 }

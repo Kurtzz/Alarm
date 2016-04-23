a65c51b27399f30a1f7c7c03fc4f6a1804d474c8
Kurtzz <przemyslaw.kurc@gmail.com>
2016-04-23 12:01:17 +0200

    Add Intent which starts SettingsActivity

--- app/src/main/java/pl/agh/ki/io/alarm/alarm/MainActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/alarm/MainActivity.java
@@ -1,5 +1,6 @@
 package pl.agh.ki.io.alarm.alarm;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -66,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 

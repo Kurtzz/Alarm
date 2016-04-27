85677746d9dca6dff3d32d9c562b2685430b09f0
Kurtzz <przemyslaw.kurc@gmail.com>
2016-04-27 21:02:26 +0200

    Add Send Message Activity

---
+++ app/src/main/java/pl/agh/ki/io/alarm/alarm/SendMessageActivity.java
@@ -0,0 +1,15 @@
+package pl.agh.ki.io.alarm.alarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SendMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+    }
+}

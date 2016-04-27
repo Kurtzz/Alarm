2fb44dbfe35d3a2ae455469857fb132da0ecfc9a
Kurtzz <przemyslaw.kurc@gmail.com>
2016-04-27 20:32:10 +0200

    Add OnClickListeners to Buttons

--- app/src/main/java/pl/agh/ki/io/alarm/alarm/AddFriendActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/alarm/AddFriendActivity.java
@@ -2,12 +2,22 @@ package pl.agh.ki.io.alarm.alarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AddFriendActivity extends AppCompatActivity {
+public class AddFriendActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+
+        Button addFriendButton = (Button) findViewById(R.id.add_friend_button);
+        addFriendButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/pl/agh/ki/io/alarm/alarm/CreateGroupActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/alarm/CreateGroupActivity.java
@@ -2,12 +2,22 @@ package pl.agh.ki.io.alarm.alarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class CreateGroupActivity extends AppCompatActivity {
+public class CreateGroupActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+
+        Button createGroupButton = (Button) findViewById(R.id.create_group_button);
+        createGroupButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }

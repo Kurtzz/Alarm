2433ee498a39693fdb17a129fa4bdeca09f06ba9
Kurtzz <przemyslaw.kurc@gmail.com>
2016-04-27 21:04:02 +0200

    Add Spinner and OnClickListener to SendMessageActivity

--- app/src/main/java/pl/agh/ki/io/alarm/alarm/SendMessageActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/alarm/SendMessageActivity.java
@@ -2,14 +2,30 @@ package pl.agh.ki.io.alarm.alarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class SendMessageActivity extends AppCompatActivity {
+public class SendMessageActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.levels_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Button sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }

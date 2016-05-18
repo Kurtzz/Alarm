ac627adcb9572d49e57b4bd63f3ef5f9e5cf9751
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-18 13:42:17 +0200

    Modify Group model

--- app/src/main/java/pl/agh/ki/io/alarm/sqlite/model/Group.java
+++ app/src/main/java/pl/agh/ki/io/alarm/sqlite/model/Group.java
@@ -1,12 +1,45 @@
 package pl.agh.ki.io.alarm.sqlite.model;
 
-import java.util.List;
-
 /**
  * Created by P on 18.05.2016.
  */
 public class Group {
     private int id;
     private String groupName;
-    private List<Friend> friends;
+    private int groupLevel;
+
+
+    public Group() {
+
+    }
+
+    public Group(int id, String groupName, int groupLevel) {
+        this.id = id;
+        this.groupName = groupName;
+        this.groupLevel = groupLevel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public int getGroupLevel() {
+        return groupLevel;
+    }
+
+    public void setGroupLevel(int groupLevel) {
+        this.groupLevel = groupLevel;
+    }
 }

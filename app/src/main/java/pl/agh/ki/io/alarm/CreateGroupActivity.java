c6db681576c71793fe5e7d47940e7daaa3a82e4d
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-18 18:36:07 +0200

    Read friends from Database

--- app/src/main/java/pl/agh/ki/io/alarm/CreateGroupActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/CreateGroupActivity.java
@@ -1,8 +1,8 @@
 package pl.agh.ki.io.alarm;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -10,23 +10,39 @@ import android.widget.ListView;
 import java.util.List;
 
 import pl.agh.ki.io.alarm.alarm.R;
+import pl.agh.ki.io.alarm.sqlite.helper.DatabaseHelper;
+import pl.agh.ki.io.alarm.sqlite.model.Friend;
 
 public class CreateGroupActivity extends AppCompatActivity implements View.OnClickListener {
     private ListView friendList;
     private FriendListAdapter friendListAdapter;
-    private List<String> friends;
+
+    private DatabaseHelper databaseHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
 
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+
         Button createGroupButton = (Button) findViewById(R.id.create_group_button);
         createGroupButton.setOnClickListener(this);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        List<Friend> friends = databaseHelper.getFriends();
+
+        friendList = (ListView) findViewById(R.id.friendList_createGroup);
+        friendListAdapter = new FriendListAdapter(getApplicationContext(), R.layout.friend_list_item_multi, friends);
+
+        friendListAdapter.setArrayList(friends);
+        friendList.setAdapter(friendListAdapter);
     }
+
+
 
     @Override
     public void onClick(View v) {

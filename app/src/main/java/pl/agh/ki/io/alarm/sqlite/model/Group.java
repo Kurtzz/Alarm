8436a742329a46a14f5d37dbde53739c9b840971
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-18 13:12:28 +0200

    Create Group model

---
+++ app/src/main/java/pl/agh/ki/io/alarm/sqlite/model/Group.java
@@ -0,0 +1,12 @@
+package pl.agh.ki.io.alarm.sqlite.model;
+
+import java.util.List;
+
+/**
+ * Created by P on 18.05.2016.
+ */
+public class Group {
+    private int id;
+    private String groupName;
+    private List<Friend> friends;
+}

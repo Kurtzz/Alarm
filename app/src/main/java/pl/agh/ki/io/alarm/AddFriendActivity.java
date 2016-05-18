a629410eda8e3e6599ffab9295fca73f8b376a9f
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-18 15:37:53 +0200

    Insert Friend into database from UI

--- app/src/main/java/pl/agh/ki/io/alarm/AddFriendActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/AddFriendActivity.java
@@ -1,22 +1,33 @@
 package pl.agh.ki.io.alarm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import pl.agh.ki.io.alarm.alarm.R;
+import pl.agh.ki.io.alarm.sqlite.helper.DatabaseHelper;
+import pl.agh.ki.io.alarm.sqlite.model.Friend;
 
 public class AddFriendActivity extends AppCompatActivity implements View.OnClickListener {
+    private DatabaseHelper databaseHelper;
+    private EditText editText;
+    private static final int MAX_LEVEL = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
 
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+
         Button addFriendButton = (Button) findViewById(R.id.add_friend_button);
         addFriendButton.setOnClickListener(this);
+
+        editText = (EditText) findViewById(R.id.friendNick_editText);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -24,6 +35,13 @@ public class AddFriendActivity extends AppCompatActivity implements View.OnClick
 
     @Override
     public void onClick(View v) {
+        Friend friend = new Friend();
+        friend.setNick(editText.getText().toString());
+        friend.setLevel(MAX_LEVEL);
+        editText.setText("");
+        databaseHelper.createFriend(friend);
 
+        Intent intent = new Intent(v.getContext(), MainActivity.class);
+        startActivity(intent);
     }
 }

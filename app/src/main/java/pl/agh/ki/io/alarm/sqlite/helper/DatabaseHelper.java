9a975d4601a5711a03ae336915d9a4759ca50ae8
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-18 13:15:51 +0200

    Init DatabaseHelper

---
+++ app/src/main/java/pl/agh/ki/io/alarm/sqlite/helper/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package pl.agh.ki.io.alarm.sqlite.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by P on 18.05.2016.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    //Database name and version
+    private static final String DATABASE_NAME = "alarmDatabase";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}

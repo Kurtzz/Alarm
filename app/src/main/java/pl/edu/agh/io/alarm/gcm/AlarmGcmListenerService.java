a10f510bfe413163a7b1c60052b1327c21211dad
nachteil <nachteil@gmail.com>
2016-05-18 14:19:07 +0200

    Created stub for Instance registration

--- app/src/main/java/pl/edu/agh/io/alarm/gcm/AlarmGcmListenerService.java
+++ app/src/main/java/pl/edu/agh/io/alarm/gcm/AlarmGcmListenerService.java
@@ -1,6 +1,18 @@
 package pl.edu.agh.io.alarm.gcm;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import com.google.android.gms.gcm.GcmListenerService;
 
 public class AlarmGcmListenerService extends GcmListenerService{
+
+    private static String TAG = AlarmGcmListenerService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+        Log.d(TAG, "From: " + from);
+        Log.d(TAG, "Message: " + message);
+    }
 }

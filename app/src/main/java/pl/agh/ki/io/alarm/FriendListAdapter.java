43994daa105b4c236aa1db5910ec7674ff43e075
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-21 12:49:37 +0200

    Adapt FriendListAdapter to Multi Choice List

--- app/src/main/java/pl/agh/ki/io/alarm/FriendListAdapter.java
+++ app/src/main/java/pl/agh/ki/io/alarm/FriendListAdapter.java
@@ -1,10 +1,13 @@
 package pl.agh.ki.io.alarm;
 
 import android.content.Context;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.List;
@@ -17,18 +20,21 @@ import pl.agh.ki.io.alarm.sqlite.model.Friend;
  */
 public class FriendListAdapter extends ArrayAdapter<Friend> {
     private Context context;
-    public List<Friend> friendList;
-    int textViewResourceId;
+    private List<Friend> friendList;
+    private int textViewResourceId;
+    private SparseBooleanArray checkedItems;
+
 
     public FriendListAdapter(Context context, int textViewResourceId, List<Friend> friends) {
         super(context, textViewResourceId);
         this.context = context;
         this.friendList = friends;
         this.textViewResourceId = textViewResourceId;
+        this.checkedItems = new SparseBooleanArray();
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         Holder holder;
 
         if (convertView == null) {
@@ -39,6 +45,13 @@ public class FriendListAdapter extends ArrayAdapter<Friend> {
                 holder.nick = (TextView) convertView.findViewById(R.id.friendListItem_nickTextView);
             } else {
                 holder.nick = (TextView) convertView.findViewById(R.id.friendListItemMulti_nickTextView);
+                holder.checkBox = (CheckBox) convertView.findViewById(R.id.friendListItemMulti_checkBox);
+                holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        checkedItems.append(position, isChecked);
+                    }
+                });
             }
             convertView.setTag(holder);
         } else {
@@ -65,7 +78,12 @@ public class FriendListAdapter extends ArrayAdapter<Friend> {
         notifyDataSetChanged();
     }
 
+    public SparseBooleanArray getCheckedItems() {
+        return checkedItems;
+    }
+
     private class Holder {
         public TextView nick;
+        public CheckBox checkBox;
     }
 }

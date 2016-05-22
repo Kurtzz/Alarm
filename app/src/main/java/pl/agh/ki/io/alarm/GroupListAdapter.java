063d102aee086232833909dc290a5be457bbd84b
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-22 11:58:22 +0200

    Implement GroupListAdapter

--- app/src/main/java/pl/agh/ki/io/alarm/GroupListAdapter.java
+++ app/src/main/java/pl/agh/ki/io/alarm/GroupListAdapter.java
@@ -1,42 +1,58 @@
 package pl.agh.ki.io.alarm;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import pl.agh.ki.io.alarm.alarm.R;
+import pl.agh.ki.io.alarm.sqlite.model.Friend;
+import pl.agh.ki.io.alarm.sqlite.model.Group;
 
 /**
  * Created by P on 18.05.2016.
  */
 public class GroupListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<Group> groupList;
+
+    public GroupListAdapter(Context context, List<Group> groups) {
+        this.context = context;
+        this.groupList = groups;
+    }
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return groupList.size();
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return 0;
+        return groupList.get(groupPosition).getFriends().size();
     }
 
     @Override
     public Object getGroup(int groupPosition) {
-        return null;
+        return groupList.get(groupPosition);
     }
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return null;
+        return groupList.get(groupPosition).getFriends().get(childPosition);
     }
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return groupPosition;
     }
 
     @Override
     public long getChildId(int groupPosition, int childPosition) {
-        return 0;
+        return childPosition;
     }
 
     @Override
@@ -46,16 +62,57 @@ public class GroupListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+        HeaderHolder holder;
+
+        if (convertView == null) {
+            holder = new HeaderHolder();
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.group_list_item_header, null);
+            holder.groupName = (TextView) convertView.findViewById(R.id.groupName);
+            convertView.setTag(holder);
+        } else {
+            holder = (HeaderHolder) convertView.getTag();
+        }
+        Group group = (Group) getGroup(groupPosition);
+        holder.groupName.setText(group.getGroupName());
+
+        return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        FriendHolder holder;
+
+        if (convertView == null) {
+            holder = new FriendHolder();
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.group_list_item_child, null);
+            holder.nick = (TextView) convertView.findViewById(R.id.memberOfGroup);
+            convertView.setTag(holder);
+        } else {
+            holder = (FriendHolder) convertView.getTag();
+        }
+        Friend friend = (Friend) getChild(groupPosition, childPosition);
+        holder.nick.setText(friend.getNick());
+
+        return convertView;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return false;
+    }
+
+    public void setArrayList(List<Group> groups) {
+        this.groupList = groups;
+        notifyDataSetChanged();
+    }
+
+    private class HeaderHolder {
+        public TextView groupName;
+    }
+
+    private class FriendHolder {
+        public TextView nick;
     }
 }

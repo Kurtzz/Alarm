2ae01c1f9c8c06ff073803ebb7becd1e0b763df5
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-23 21:25:56 +0200

    Override equals nad hashCode methods

--- app/src/main/java/pl/agh/ki/io/alarm/sqlite/model/Friend.java
+++ app/src/main/java/pl/agh/ki/io/alarm/sqlite/model/Friend.java
@@ -51,4 +51,20 @@ public class Friend {
     public void setBlocked(boolean blocked) {
         isBlocked = blocked;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Friend friend = (Friend) o;
+
+        return getId() == friend.getId();
+
+    }
+
+    @Override
+    public int hashCode() {
+        return getId();
+    }
 }

7c48371adb6750b671f89a09d757b2cc7a53cdef
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-27 13:11:23 +0200

    Add imageButton to holder, add onClickListener to imageButton.

--- app/src/main/java/pl/agh/ki/io/alarm/DefaultFriendListAdapter.java
+++ app/src/main/java/pl/agh/ki/io/alarm/DefaultFriendListAdapter.java
@@ -1,9 +1,11 @@
 package pl.agh.ki.io.alarm;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import pl.agh.ki.io.alarm.alarm.R;
@@ -27,17 +29,28 @@ public class DefaultFriendListAdapter extends AbstractFriendListAdapter {
             LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = vi.inflate(getTextViewResourceId(), null);
             holder.nick = (TextView) convertView.findViewById(R.id.friendListItem_nickTextView);
+            holder.imageButton = (ImageButton) convertView.findViewById(R.id.friendListItem_imageButton);
             convertView.setTag(holder);
         } else {
             holder = (Holder) convertView.getTag();
         }
-        Friend friend = getItem(position);
+
+        final Friend friend = getItem(position);
         holder.nick.setText(friend.getNick());
+        holder.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SendMessageActivity.class);
+                intent.putExtra(FriendsFragment.EXTRA_FRIEND_ID, friend.getId());
+                getContext().startActivity(intent);
+            }
+        });
 
         return convertView;
     }
 
     private class Holder {
         public TextView nick;
+        public ImageButton imageButton;
     }
 }

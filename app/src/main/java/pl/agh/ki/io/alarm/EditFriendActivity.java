28418dcab67cac6fb661f76ef961f4580c9a5d3e
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-29 00:05:19 +0200

    Bind data to EditFriendActivity

--- app/src/main/java/pl/agh/ki/io/alarm/EditFriendActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/EditFriendActivity.java
@@ -3,15 +3,48 @@ package pl.agh.ki.io.alarm;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import pl.agh.ki.io.alarm.alarm.R;
+import pl.agh.ki.io.alarm.sqlite.helper.DatabaseHelper;
+import pl.agh.ki.io.alarm.sqlite.model.Friend;
 
 public class EditFriendActivity extends AppCompatActivity implements View.OnClickListener {
+    private Spinner spinner;
+    private Friend friend;
+    private Button button;
+    private CheckBox checkBox;
+
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_friend);
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+
+        friend = databaseHelper.getFriend(getIntent().getIntExtra(SendMessageActivity.EXTRA_ID, 0));
+
+        TextView textView = (TextView) findViewById(R.id.editFriend_nickTextView);
+        textView.setText(friend.getNick());
+
+        checkBox = (CheckBox) findViewById(R.id.editFriend_checkBox);
+        checkBox.setChecked(friend.isBlocked());
+
+        spinner = (Spinner) findViewById(R.id.editFriend_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.alarmLevelsArray, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(friend.getLevel() - 1);
+
+        button = (Button) findViewById(R.id.editFriend_saveButton);
+        button.setOnClickListener(this);
     }
 
     @Override

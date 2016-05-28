a018ccc4b736614bbddc119db83695c687c3447a
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-29 00:17:19 +0200

    Create EditGroupActivity

---
+++ app/src/main/java/pl/agh/ki/io/alarm/EditGroupActivity.java
@@ -0,0 +1,15 @@
+package pl.agh.ki.io.alarm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import pl.agh.ki.io.alarm.alarm.R;
+
+public class EditGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_group);
+    }
+}

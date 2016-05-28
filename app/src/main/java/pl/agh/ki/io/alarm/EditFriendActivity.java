46a89b9cdd58e15aebd54c66a26cf2a40b25298d
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-29 00:04:00 +0200

    Create EditGroupActivity

---
+++ app/src/main/java/pl/agh/ki/io/alarm/EditFriendActivity.java
@@ -0,0 +1,21 @@
+package pl.agh.ki.io.alarm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import pl.agh.ki.io.alarm.alarm.R;
+
+public class EditFriendActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_friend);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}

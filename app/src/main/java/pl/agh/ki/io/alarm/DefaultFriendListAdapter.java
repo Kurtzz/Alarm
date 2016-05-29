7d781259e36735d9e00a516beb2d41b2e69afa39
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-29 15:24:00 +0200

    Set different text color depend on friend is block or not

--- app/src/main/java/pl/agh/ki/io/alarm/DefaultFriendListAdapter.java
+++ app/src/main/java/pl/agh/ki/io/alarm/DefaultFriendListAdapter.java
@@ -2,6 +2,7 @@ package pl.agh.ki.io.alarm;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,6 +38,11 @@ public class DefaultFriendListAdapter extends AbstractFriendListAdapter {
 
         final Friend friend = getItem(position);
         holder.nick.setText(friend.getNick());
+        if (friend.isBlocked()) {
+            holder.nick.setTextColor(Color.GRAY);
+        } else {
+            holder.nick.setTextColor(Color.BLACK);
+        }
         holder.imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

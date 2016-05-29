1875044bfaf4a737fb0e6796d51aa106ff8f7f82
Kurtzz <przemyslaw.kurc@gmail.com>
2016-05-29 14:39:07 +0200

    Implement onClick save button listener

--- app/src/main/java/pl/agh/ki/io/alarm/EditGroupActivity.java
+++ app/src/main/java/pl/agh/ki/io/alarm/EditGroupActivity.java
@@ -2,12 +2,15 @@ package pl.agh.ki.io.alarm;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseBooleanArray;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import pl.agh.ki.io.alarm.alarm.R;
@@ -15,7 +18,7 @@ import pl.agh.ki.io.alarm.sqlite.helper.DatabaseHelper;
 import pl.agh.ki.io.alarm.sqlite.model.Friend;
 import pl.agh.ki.io.alarm.sqlite.model.Group;
 
-public class EditGroupActivity extends AppCompatActivity {
+public class EditGroupActivity extends AppCompatActivity implements View.OnClickListener {
     private Spinner spinner;
     private Group group;
     private Button button;
@@ -58,5 +61,25 @@ public class EditGroupActivity extends AppCompatActivity {
         }
 
         button = (Button) findViewById(R.id.editGroup_saveButton);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        SparseBooleanArray checkedItems = friendListAdapter.getCheckedItems();
+        List<Friend> checkedFriends = new ArrayList<>();
+
+        for (int i = 0; i < checkedItems.size(); i++) {
+            int key = checkedItems.keyAt(i);
+            if (checkedItems.valueAt(i)) {
+                checkedFriends.add(friendListAdapter.getItem(key));
+            }
+        }
+
+        group.setGroupLevel(spinner.getSelectedItemPosition() + 1);
+        group.setFriends(checkedFriends);
+        databaseHelper.updateGroup(group);
+
+        onBackPressed();
     }
 }

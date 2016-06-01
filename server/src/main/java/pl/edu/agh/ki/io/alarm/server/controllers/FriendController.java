48c4bed1041ecffc4031861980740c143bd0c407
nachteil <nachteil@gmail.com>
2016-06-01 18:18:28 +0200

    Server side changes for group invitations

--- server/src/main/java/pl/edu/agh/ki/io/alarm/server/controllers/FriendController.java
+++ server/src/main/java/pl/edu/agh/ki/io/alarm/server/controllers/FriendController.java
@@ -5,10 +5,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.agh.ki.io.alarm.domain.User;
-import pl.edu.agh.ki.io.alarm.server.communication.GcmMessage;
-import pl.edu.agh.ki.io.alarm.server.communication.GcmMessageData;
-import pl.edu.agh.ki.io.alarm.server.communication.GoogleCloudService;
-import pl.edu.agh.ki.io.alarm.server.communication.MessageType;
+import pl.edu.agh.ki.io.alarm.server.communication.*;
 import pl.edu.agh.ki.io.alarm.server.registry.UserRepository;
 
 import java.util.Map;
@@ -39,6 +36,44 @@ public class FriendController {
         }
 
     }
+
+    @RequestMapping(value = "/invitation/accept/")
+    public void acceptInvitation(@RequestBody Map<String, String> body) throws JsonProcessingException, UnirestException {
+
+        String senderToken = body.get("SENDER_TOKEN");
+        String inviteeUid = body.get("INVITEE_UID");
+        String groupId = body.get("GROUP_ID");
+
+        String inviteeNick = userRepository.get(inviteeUid).getNick();
+
+        GcmMessage message = createInvitationResponseMessage(senderToken, groupId, inviteeNick, InvitationResponse.ACCEPTED);
+
+        gcm.send(message);
+    }
+
+    private GcmMessage createInvitationResponseMessage(String senderToken, String groupId, String inviteeNick, InvitationResponse response) {
+        GcmMessage message = new GcmMessage();
+        message.setTo(senderToken);
+        GcmMessageData data = new GcmMessageData();
+        data.setMessageType(MessageType.INVITATION_RESPONSE);
+        data.setInvitationResponse(response);
+        data.setSenderNick(inviteeNick);
+        data.setGroupName(groupId);
+        message.setData(data);
+        return message;
+    }
+
+    @RequestMapping(value = "/invitation/decline/")
+    public void declineInvitation(@RequestBody Map<String, String> body) throws JsonProcessingException, UnirestException {
+        
+        String senderToken = body.get("SENDER_TOKEN");
+        String inviteeUid = body.get("INVITEE_UID");
+        String groupId = body.get("GROUP_ID");
+        String inviteeNick = userRepository.get(inviteeUid).getNick();
+        GcmMessage message = createInvitationResponseMessage(senderToken, groupId, inviteeNick, InvitationResponse.DECLINED);
+        gcm.send(message);
+    }
+
 
     private GcmMessage composeInvitationMessage(String inviteeUid, String groupId, User sender) {
         String inviteeToken = userRepository.get(inviteeUid).getToken();

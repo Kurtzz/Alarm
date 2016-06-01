03c864ea9c5a8dccfcf70e148b0b0849088fbf9e
nachteil <nachteil@gmail.com>
2016-06-01 15:44:25 +0200

    Improved coverage

--- server/src/test/java/pl/edu/agh/ki/io/alarm/server/communication/MessageSerializerTest.java
+++ server/src/test/java/pl/edu/agh/ki/io/alarm/server/communication/MessageSerializerTest.java
@@ -1,7 +1,42 @@
 package pl.edu.agh.ki.io.alarm.server.communication;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MessageSerializerTest {
+
+    private MessageSerializer instance;
+
+    @Before
+    public void before() {
+        this.instance = new MessageSerializer();
+    }
+
+    @Test
+    public void shouldSerializeMessage() throws JsonProcessingException {
+
+        // given
+        GcmMessage message = new GcmMessage();
+        message.setTo("token");
+
+        GcmMessageData data = new GcmMessageData();
+        data.setMessage("message");
+        data.setGroupName("someGroup");
+        data.setSenderNick("sender");
+        data.setSenderUID("uid");
+
+        message.setData(data);
+
+        // when
+        String serializedMessage = instance.serialize(message);
+
+        // then
+        assertNotNull(serializedMessage);
+        String expected = "{\"data\":{\"message\":\"message\",\"senderNick\":\"sender\",\"senderUID\":\"uid\",\"groupName\":\"someGroup\"},\"to\":\"token\"}";
+        assertEquals(serializedMessage.replace("\\s*", ""), expected);
+    }
 
 }

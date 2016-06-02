6132e095f9eece3bfad2158832048308d58f6096
nachteil <nachteil@gmail.com>
2016-06-02 17:54:43 +0200

    Change activity order

--- app/src/main/java/pl/edu/agh/io/alarm/gcm/id/AlarmInstanceListenerService.java
+++ app/src/main/java/pl/edu/agh/io/alarm/gcm/id/AlarmInstanceListenerService.java
@@ -1,13 +1,17 @@
 package pl.edu.agh.io.alarm.gcm.id;
 
 import android.content.Intent;
+import android.util.Log;
 
 import com.google.android.gms.iid.InstanceIDListenerService;
 
 public class AlarmInstanceListenerService extends InstanceIDListenerService {
 
+    private static final String TAG = "InstanceListenerService";
+
     @Override
     public void onTokenRefresh() {
+        Log.w(TAG, "onTokenRefresh");
         Intent intent = new Intent(this, InstanceRegistrationIntent.class);
         startService(intent);
     }
--- app/src/main/java/pl/edu/agh/io/alarm/ui/activities/StartActivity.java
+++ app/src/main/java/pl/edu/agh/io/alarm/ui/activities/StartActivity.java
@@ -1,17 +1,28 @@
 package pl.edu.agh.io.alarm.ui.activities;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 import pl.edu.agh.io.alarm.R;
+import pl.edu.agh.io.alarm.gcm.Constants;
+import pl.edu.agh.io.alarm.gcm.id.InstanceRegistrationIntent;
 import pl.edu.agh.io.alarm.middleware.Middleware;
 
 /**
@@ -19,6 +30,11 @@ import pl.edu.agh.io.alarm.middleware.Middleware;
  */
 public class StartActivity extends Activity implements View.OnClickListener {
 
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private boolean isReceiverRegistered;
 
     private boolean middlewareIsBound;
     private Middleware middlewareService;
@@ -47,6 +63,7 @@ public class StartActivity extends Activity implements View.OnClickListener {
                     middlewareService.setNickname(loginText.getText().toString());
                     Intent intent = new Intent(this, MainActivity.class);
                     startActivity(intent);
+                    registerIdentityWithGoogle();
                 }
                 break;
         }
@@ -86,4 +103,52 @@ public class StartActivity extends Activity implements View.OnClickListener {
 
     }
 
+
+    private void registerIdentityWithGoogle() {
+        Log.i(TAG, "Initializing token generation...");
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(Constants.TOKEN_REGISTERED, false);
+                Log.i(TAG, "Received broadcast: tokenSent - " + sentToken);
+            }
+        };
+
+        // Registering BroadcastReceiver
+        registerReceiver();
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Log.i(TAG, "Starting ID intent...");
+            Intent intent = new Intent(this, InstanceRegistrationIntent.class);
+            startService(intent);
+        }
+    }
+
+    private void registerReceiver(){
+        if(!isReceiverRegistered) {
+            LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                    new IntentFilter(Constants.REGISTRATION_COMPLETE));
+            isReceiverRegistered = true;
+        }
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
 }
